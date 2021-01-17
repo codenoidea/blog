eb0516b4753ab73c949279c8869c7a0c075e0e9c
codenoidea <codenoidea@gmail.com>
2021-01-17 17:04:48 +0900

    feat(#1): jwt추가

---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+'use strict'
+module.exports = {
+  ACCESS_TOKEN_SECRET: 'accessTokenSecret',
+  ACCESS_TOKEN_LIFE: 120,
+  REFRESH_TOKEN_SECRET: 'refreshTokenSecret',
+  REFRESH_TOKEN_LIFE: 86400,
+}
