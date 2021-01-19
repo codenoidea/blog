9c3e7e94b6a25931a070d619b015389df514303f
codenoidea <codenoidea@gmail.com>
2021-01-19 23:01:12 +0900

    feat(#3): 에러코드 추가

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -15,12 +15,16 @@ class GeneralError extends Error {
     if (this instanceof NotFound) {
       return 404;
     }
+    if (this instanceof Unauthorized) {
+      return 401;
+    }
     return 500;
   }
 }
 
 class BadRequest extends GeneralError {}
 class NotFound extends GeneralError {}
+class Unauthorized extends GeneralError {}
 
 module.exports = {
   GeneralError,
