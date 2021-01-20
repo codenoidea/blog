d8d2bbaacc500409594f327fa3ce3947dad7f8a2
codenoidea <codenoidea@gmail.com>
2021-01-20 23:17:58 +0900

    fix(#5): 로그인 비밀번호 확인

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -6,16 +6,19 @@ import {
 } from '../utils/errors';
 import Bcrypt from '../utils/bcrypt';
 
+async function comparePassword(user, result) {
+  if (!await Bcrypt.compare(user.password, result.password)) {
+    throw new BadRequest('비밀번호를 확인해주세요');
+  }
+}
+
 class UserService {
   async SignIn(user) {
     try {
       const userResult = await UserModel.SignIn({
         name: user.name
       });
-      console.log(userResult[0].password, user.password)
-      if (await Bcrypt.compare(user.password, userResult[0].password)) {
-        throw new BadRequest('비밀번호를 확인해주세요');
-      }
+      await comparePassword(user, userResult[0]);
       return userResult;
     } catch (error) {
       throw error;
