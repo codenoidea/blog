570145fea71507895bef7fe3ed23bee097fea8d7
codenoidea <codenoidea@gmail.com>
2021-01-21 23:35:05 +0900

    feat(#7): 도커수정

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   // mongoDb: 'mongodb://mongo:27017/blog',
-  mongoDb: 'mongodb://127.0.0.1:27017/blog',
+  mongoDb: process.env.mongoDb || 'mongodb://127.0.0.1:27017/blog',
   ACCESS_TOKEN_SECRET: 'accessTokenSecret',
   ACCESS_TOKEN_LIFE: 120,
   REFRESH_TOKEN_SECRET: 'refreshTokenSecret',
